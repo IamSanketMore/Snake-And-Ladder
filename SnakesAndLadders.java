e790d0c5ecce786472daabfc73e9ef99d7831004
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-13 01:39:28 +0530

    UC-5:- Reach Exact Winning Position 100

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -1,40 +1,53 @@
 public class SnakesAndLadders
 {
-	public static final int Ending_Point=100;
-	
-	public static void main(String args[])
+	public static final int Ladder=1;
+	public static final int Snake=2;
+
+	public static void main(String args[]) 
 	{
 		int Pos=0;
-		final int Ladder=1;
-		final int Snake=2;
-		
-		while (Pos <= 100)
+		while (Pos < 100)
 		{
 			int Option = (int)Math.floor(Math.random() * 10) % 3; 
 			int Dice = (int)(Math.floor(Math.random() * 10) % 6) + 1;
-			
-			switch(Option)
+
+			if(Option == Ladder)
 			{
-				case Ladder:
-						Pos=Pos+Dice;
-						System.out.println("****** You Got a Ladder ******\nYou Move Ahead By " +Dice+ " To " +Pos+ " Positions");
-						
-						break;
-			
-				case Snake:
-						if(Pos>=0)
+					Pos=Pos+Dice;
+					System.out.println("****** You Got a Ladder ******\nYou Move Ahead By " +Dice+ " To " +Pos+ " Positions");
+
+					if(Pos < 0 || Pos > 106)
+					{
+						System.out.println("Error");
+					}
+					else if(Pos  > 100)
+					{
+						Pos = Pos - Dice;
+						System.out.println("You Cant Jump ");
+					}
+					else if( Pos == 100)
+					{
+						System.out.println("\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" );
+						System.out.println("************************** You Win The Game *****************************");
+						System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n" );
+						break ;
+					}
+			}
+			else if(Option == 2)
+			{
+						if(Pos > 0)
 						{
-							Pos=Pos-Dice;
+							Pos = Pos - Dice;
 							System.out.println("****** You Got a Snake ******\nYou Move Behind By " +Dice+ " To " +Pos+ "Positions");
 						}
 						else
 						{
 							System.out.println("Player Position moves below '0' \nRe-Start Game From "+Pos);
 						}
-						break;
-			
-				default:
-						System.out.println("Player Does not Play His turn \nStay in the Same Position:- "+Pos);
+			}
+			else
+			{
+						System.out.println("Player Does not Play His Turn \nStay in the Same Position:- "+Pos);
 			}
 		}
 	}
