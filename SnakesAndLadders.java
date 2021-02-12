1a02fe350ff1cded5424019b781048eb9bc66cd0
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-13 01:15:35 +0530

    UC-1:- Play With Single Player Start with 0

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -1,7 +1,16 @@
 public class SnakesAndLadders
 {
-	public static void main(String args[])
+	public static final int Starting_Point=0;
+	public static final int Ending_Point=100;
+	public static void main(String args [])
 	{
-		System.out.println("Welcomw to Snakes And Ladders Game");
+		System.out.println("***************************************************");
+		System.out.println("Welcome to Snake And Ladder Game");
+		System.out.println("***************************************************");
+		int Player_1=0;
+		
+		System.out.println("Game Start Point is:-"+Starting_Point+ "\nEnd point is:-"+Ending_Point);
+		System.out.println("\nPlayer Intitial Starting Point is :-"  +Player_1);
 	}
 }
+
